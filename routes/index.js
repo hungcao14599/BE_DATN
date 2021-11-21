7e34b25fd8ee1a3b18db77615af1efb6f82177e6
hungcao14599 <hungnguyen14599@gmail.com>
2021-11-21 23:10:50 +0700

    BE/post-comment

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ import authRouter from "./auths";
 import userRouter from "./users";
 import postRouter from "./posts";
 import postMoodRouter from "./postMood";
+import postCommentRouter from "./postComment";
 
 var router = express.Router();
 
@@ -10,5 +11,6 @@ router.use("/api/auth", authRouter);
 router.use("/api/users", userRouter);
 router.use("/api/posts", postRouter);
 router.use("/api/post-mood", postMoodRouter);
+router.use("/api/post-comment", postCommentRouter);
 
 export default router;
