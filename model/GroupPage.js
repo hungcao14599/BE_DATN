c7919875a6634c22d5de769024f4c477061af70e
hungnguyen14599 <hungnguyen14599@gmail.com>
2021-12-16 00:13:27 +0700

    BE/Group-Page

--- model/GroupPage.js
+++ model/GroupPage.js
@@ -1,57 +1,63 @@
 import Sequelize from "sequelize";
 import { sequelize } from "../config/connect";
 const GroupPage = sequelize.define(
-    "group_pages", {
-        id: {
-            field: "id",
-            type: Sequelize.INTEGER,
-            primaryKey: true,
-            autoIncrement: true,
-        },
-        name: {
-            field: "name",
-            type: Sequelize.STRING,
-        },
-        description: {
-            field: "description",
-            type: Sequelize.STRING,
-        },
-        status: {
-            field: "status",
-            type: Sequelize.INTEGER,
-        },
-        avatar: {
-            field: "avatar",
-            type: Sequelize.STRING,
-        },
-        coverImage: {
-            field: "cover_image",
-            type: Sequelize.STRING,
-        },
-        isDelete: {
-            field: "is_delete",
-            type: Sequelize.BOOLEAN,
-        },
-        createdBy: {
-            field: "created_by",
-            type: Sequelize.INTEGER,
-        },
-        createdAt: {
-            field: "created_at",
-            type: Sequelize.DATE,
-        },
-        updatedAt: {
-            field: "updated_at",
-            type: Sequelize.DATE,
-        },
-        updatedBy: {
-            field: "updated_by",
-            type: Sequelize.INTEGER,
-        },
-    }, {
-        tableName: "group_pages",
-        timestamps: false,
-    }
+  "group_pages",
+  {
+    id: {
+      field: "id",
+      type: Sequelize.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    name: {
+      field: "name",
+      type: Sequelize.STRING,
+    },
+    description: {
+      field: "description",
+      type: Sequelize.STRING,
+    },
+    caption: {
+      field: "caption",
+      type: Sequelize.STRING,
+    },
+    status: {
+      field: "status",
+      type: Sequelize.INTEGER,
+    },
+    avatar: {
+      field: "avatar",
+      type: Sequelize.STRING,
+    },
+    coverImage: {
+      field: "cover_image",
+      type: Sequelize.STRING,
+    },
+    isDelete: {
+      field: "is_delete",
+      type: Sequelize.BOOLEAN,
+    },
+    createdBy: {
+      field: "created_by",
+      type: Sequelize.INTEGER,
+    },
+    createdAt: {
+      field: "created_at",
+      type: Sequelize.DATE,
+    },
+    updatedAt: {
+      field: "updated_at",
+      type: Sequelize.DATE,
+    },
+    updatedBy: {
+      field: "updated_by",
+      type: Sequelize.INTEGER,
+    },
+  },
+  {
+    tableName: "group_pages",
+    timestamps: false,
+  }
 );
 
 export default GroupPage;
