19de9766c2c202341ffa3ddf14c95fd86fe5f04f
hungcao14599 <hungnguyen14599@gmail.com>
2025-06-25 09:15:48 +0700

    update index file

--- api/index.js
+++ api/index.js
@@ -1,24 +1,5 @@
-const express = require("express");
+// api/index.js
 const serverless = require("serverless-http");
-const cors = require("cors");
-const swaggerUi = require("swagger-ui-express");
-const swaggerSpec = require("../swagger");
+const app = require("../app");
 
-const app = express();
-
-app.use(cors());
-app.use(express.json());
-
-app.get("/hello", (req, res) => {
-  res.json({ message: "Hello from Express on Vercel!" });
-});
-
-app.get("/", (req, res) => {
-  console.log("API gọi thành công");
-  res.json({ message: "Hello from API" });
-});
-
-app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
-
-// ❗ Xuất default như Vercel yêu cầu
 module.exports = serverless(app);
