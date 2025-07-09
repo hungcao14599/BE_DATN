cb46029df92645c507a6e6ddff228a763aeb5163
hungcao14599 <hungnguyen14599@gmail.com>
2025-07-09 11:27:35 +0700

    change connect db

--- config/db.config.js
+++ config/db.config.js
@@ -1,8 +1,8 @@
 module.exports = {
-  DB_NAME: "railway",
+  DB_NAME: "social_network_db",
   DB_USERNAME: "root",
-  PASSWORD: "gNptgiYpdHNFGwzEojdTVbRPgeUQrhkc",
-  HOST: "yamanote.proxy.rlwy.net",
+  PASSWORD: "123456789!",
+  HOST: "14.225.206.110",
   dialect: "mysql",
-  PORT: "47014",
+  PORT: "3306",
 };
